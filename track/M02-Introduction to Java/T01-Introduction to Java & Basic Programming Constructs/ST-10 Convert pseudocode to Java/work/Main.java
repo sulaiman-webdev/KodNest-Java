ac07641b8f2f262ec01e90d4996b9e4d940e3419
sulaiman-webdev <sulaimanmohamed502@gmail.com>
2026-09-10 19:55:58 +0530

    changes made for day 9

--- track/M02-Introduction to Java/T01-Introduction to Java & Basic Programming Constructs/ST-10 Convert pseudocode to Java/work/Main.java
+++ track/M02-Introduction to Java/T01-Introduction to Java & Basic Programming Constructs/ST-10 Convert pseudocode to Java/work/Main.java
@@ -5,18 +5,11 @@ public static void main(String[] args) {
 // Convert the given pseudocode into Java code
 
 int javaHoursPerDay = 2;
-
 int aptitudeHoursPerDay = 1;
-
 int numberofDays = 5;
-
 int weeklyJavaHours = javaHoursPerDay * numberofDays;
-
-
 int weeklyAptitudeHours = aptitudeHoursPerDay * numberofDays;
-
 int totalPreparationHours = weeklyJavaHours + weeklyAptitudeHours;
-
 System.out.println("Java: "+ weeklyJavaHours);
 
 System.out.println("Aptitude: "+ weeklyAptitudeHours);
---
+++ track/M02-Introduction to Java/T01-Introduction to Java & Basic Programming Constructs/ST-14 Declare, assign, print variables/work/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        String learnerName ="Salman";
+        int dailyPracticeHours = 8;
+        
+        System.out.println ( "Name : "+ learnerName);
+        System.out.println ("Practice:"+ dailyPracticeHours +" Hours");
+    }
+}
